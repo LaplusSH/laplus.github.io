1fe6b99a28f3715272ea1fd6b1250e7df70ac996
LaplusSH <lsh0629123@gmail.com>
2024-11-15 13:32:37 +0900

    Update and rename main.js to stylee.css

--- main.js
+++
@@ -1,9 +0,0 @@
-async function registerUser(username, password) {
-    const response = await fetch('https://your-app.vercel.app/register', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password })
-    });
-    const data = await response.json();
-    console.log(data.message);
-}
